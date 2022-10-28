9ce63a3d68407853e351494e8efbf1ab706f6552
Dominik <dominik.graf2001@gmail.com>
2022-10-28 16:21:34 +0200

    field

--- src/app/chat-input/chat-input.component.ts
+++ src/app/chat-input/chat-input.component.ts
@@ -3,13 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-chat-input',
   templateUrl: './chat-input.component.html',
-  styleUrls: ['./chat-input.component.scss']
+  styleUrls: ['./chat-input.component.scss'],
 })
 export class ChatInputComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
-
+  sendMessage() {}
 }
