65b63c317e34a8d16cc5ed937d558ebaba92efea
Dominik <dominik.graf2001@gmail.com>
2022-10-29 16:01:12 +0200

    new Text function

--- src/app/chat-input/chat-input.component.ts
+++ src/app/chat-input/chat-input.component.ts
@@ -10,11 +10,35 @@ export class ChatInputComponent implements OnInit {
   constructor() {}
   newMessage: string = '';
   chat = new Chat();
+  weight: boolean = false;
+  italic: boolean = false;
 
   ngOnInit(): void {}
 
+  editWeight() {
+    if (this.weight == false) {
+      this.weight = true;
+    } else {
+      this.weight = false;
+    }
+  }
+
+  editItalic() {
+    if (this.italic == false) {
+      this.italic = true;
+    } else {
+      this.italic = false;
+    }
+  }
+
   sendMessage() {
     console.log(this.newMessage);
+    this.weight = false;
+    this.italic = false;
     this.newMessage = '';
+  }
+
+  onImageUpload() {
+    console.log('test');
   }
 }
