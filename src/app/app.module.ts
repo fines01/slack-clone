f2b3dab02cfb69a84c974cb5ac54dbd605c15d5a
Dominik Niemietz <d.niemietz97@gmx.de>
2022-11-06 20:24:55 +0100

    adding AngularFireModule in app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -33,6 +33,9 @@ import { MatSelectModule } from '@angular/material/select';
 import { RemoveImgDialogComponent } from './remove-img-dialog/remove-img-dialog.component';
 import { AdjustStatusComponent } from './adjust-status/adjust-status.component';
 import { PickerModule } from '@ctrl/ngx-emoji-mart';
+import { provideAnalytics,getAnalytics,ScreenTrackingService,UserTrackingService } from '@angular/fire/analytics';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
+import { AngularFireModule } from '@angular/fire/compat';
 
 @NgModule({
   declarations: [
@@ -51,6 +54,7 @@ import { PickerModule } from '@ctrl/ngx-emoji-mart';
     AdjustStatusComponent,
   ],
   imports: [
+    
     BrowserModule,
     AppRoutingModule,
     NoopAnimationsModule,
@@ -66,12 +70,17 @@ import { PickerModule } from '@ctrl/ngx-emoji-mart';
     MatDialogModule,
     PickerModule,
     FormsModule,
+    AngularFireModule.initializeApp(environment.firebase),
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAuth(() => getAuth()),
     provideFirestore(() => getFirestore()),
     provideStorage(() => getStorage()),
+    provideAnalytics(() => getAnalytics()),
+    provideDatabase(() => getDatabase()),
   ],
-  providers: [],
+  providers: [
+    ScreenTrackingService,UserTrackingService
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { EditUserDialogComponent } from '../edit-user-dialog/edit-user-dialog.component';
 import {MatDialog} from '@angular/material/dialog';
 import { AdjustStatusComponent } from '../adjust-status/adjust-status.component';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+
 
 @Component({
   selector: 'app-user-profile',
@@ -10,9 +12,13 @@ import { AdjustStatusComponent } from '../adjust-status/adjust-status.component'
 })
 export class UserProfileComponent implements OnInit {
 
-  constructor(public dialog: MatDialog) { }
+  user: any = {}
+
+  constructor(public dialog: MatDialog,private firestore: AngularFirestore) { }
 
   ngOnInit(): void {
+    this.getUser();
+
   }
 
   openEditUser(){
@@ -21,5 +27,16 @@ export class UserProfileComponent implements OnInit {
 
   openAdjustStatus(){
     this.dialog.open(AdjustStatusComponent);
+  }
+
+  getUser(){
+    this.firestore
+    .collection('users')
+    .doc('yliSy6DITTHuWjgw4aDE')
+    .valueChanges()
+    .subscribe((user:any) => {
+      this.user = user;
+      console.log(this.user);
+    } )
   }
 }
