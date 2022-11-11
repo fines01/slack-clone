e0a1032d22c9fa42acc4768065e787850845f39c
I. Fritsch <ines.fritsch@gmail.com>
2022-11-11 19:46:35 +0100

    add route guards and get auth user from database

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './auth-components/login/login.component';
 import { RegisterComponent } from './auth-components/register/register.component';
+import { AuthGuard } from './auth.guard';
 import { MainComponent } from './main/main.component';
 
 const routes: Routes = [
   // { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'register', component: RegisterComponent},
   { path: 'login', component: LoginComponent },
-  { path: 'main', component: MainComponent },
+  { path: 'main', component: MainComponent, canActivate: [AuthGuard] },
   { path: '**', redirectTo: 'main' }
 ];
 
--- src/models/user.class.ts
+++ src/models/user.class.ts
@@ -13,19 +13,22 @@ export class User {
     isActive: boolean = false;
 
 
+    // ACHTUNG jede einzelne Property überprüfen (ob sie vorhanden ist) 
+    // sonst gibt es Fehler wenn ich nur einzelne Werte updaten will 
+    // bsp.: beim registrieren v usern: registrieren sich nur mit usernamen, email,... alle anderen Werte können säter upgedatet werden aber ich müsste ich sonst jeden einzelnen wert leer übergeben um den User in der Db anlegen zu können.
     constructor(obj?: any){
-        this.uid = obj ? obj.uid : '';
-        this.email = obj ? obj.email : '';
-        this.emailVerified = obj ? obj.emailVerified : false;
-        this.photoURL = obj ? obj.photoURL : 'src/assets/img/avatar-placeholder.png';
-        this.displayName = obj ? obj.displayName : '';
-        this.firstName = obj ? obj.firstName : '';
-        this.lastName = obj ? obj.lastName : '';
-        this.namePronunciation = obj ? obj.namePronunciation : '+ Aussprache des Namens hinzufügen';
-        this.title = obj ? obj.title : '';
-        this.phone = obj ? obj.phone : '+ Phone hinzufügen';
-        this.status = obj ? obj.status : '';
-        this.isActive = obj ? obj.isActive : false;
+        this.uid = (obj && obj.uid) ? obj.uid : '';
+        this.email = (obj && obj.email) ? obj.email : '';
+        this.emailVerified = (obj && obj.emailVerified) ? obj.emailVerified : false;
+        this.photoURL = (obj && obj.photoURL) ? obj.photoURL : 'src/assets/img/avatar-placeholder.png';
+        this.displayName = (obj && obj.displayName) ? obj.displayName : '';
+        this.firstName = (obj && obj.firstName) ? obj.firstName : '';
+        this.lastName = (obj && obj.lastName) ? obj.lastName : '';
+        this.namePronunciation = (obj && obj.namePronunciation) ? obj.namePronunciation : '+ Aussprache des Namens hinzufügen';
+        this.title = (obj && obj.title) ? obj.title : '';
+        this.phone = (obj && obj.phone) ? obj.phone : '+ Phone hinzufügen';
+        this.status = (obj && obj.status) ? obj.status : '';
+        this.isActive = (obj && obj.isActive) ? obj.isActive : false;
     }
 
     // returns userdata as a json object
